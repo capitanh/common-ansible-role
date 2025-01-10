810a7374cb034ec021de7bb8d317a19cdc707e47
Luis Novo <luis.novo@gmail.com>
2025-01-10 17:07:08 +0100

    feat: Added nvim and lazyvim install

---
+++ files/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  -- add onedark
+  { "joshdick/onedark.vim" },
+
+  -- Configure LazyVim to load onedark
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "onedark",
+    },
+  }
+}
