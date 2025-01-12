3235e9210de6b14220c32950b3fbae318afafee1
Luis Novo <luis.novo@gmail.com>
2025-01-12 12:51:02 +0100

    fix: Bumped tools versions

--- files/colorscheme.lua
+++ files/colorscheme.lua
@@ -1,8 +1,15 @@
 return {
-  -- add onedark
-  { "joshdick/onedark.vim" },
-
-  -- Configure LazyVim to load onedark
+{
+  "navarasu/onedark.nvim",
+  opts = function()
+    return {
+      style = 'darker',
+      code_style = {
+        comments = 'italic',
+      },
+    }
+    end,
+  },
   {
     "LazyVim/LazyVim",
     opts = {
